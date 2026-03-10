3649bd9c3c618b5d388c3e065632df32305c1d94
gaurav-132 <ggusain264@gmail.com>
2026-03-11 02:24:54 +0530

    Changes in backend architecture

--- apps/api/src/core/middleware/error-handler.js
+++
@@ -1,9 +0,0 @@
-import { HttpError } from '../http-error.js';
-
-export function errorHandler(error, _req, res, _next) {
-  if (error instanceof HttpError) {
-    return res.status(error.statusCode).json({ message: error.message });
-  }
-
-  return res.status(500).json({ message: 'Internal server error' });
-}
